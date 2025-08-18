414ddb89065d4756c0be9a115d5525242dba95a7
vijaymaurya <vijaykushwahaji1415@gmail.com>
2025-08-18 14:41:18 +0530

    some add logingpage

--- site/src/jsMain/kotlin/com/indusjs/statustracker/pages/LogingPage.kt
+++ site/src/jsMain/kotlin/com/indusjs/statustracker/pages/LogingPage.kt
@@ -1,106 +1,58 @@
 package com.indusjs.statustracker.pages
-/*
-import androidx.compose.runtime.Composable
-import com.varabyte.kobweb.compose.css.TextAlign
-import com.varabyte.kobweb.compose.css.WhiteSpace
-import com.varabyte.kobweb.compose.dom.svg.Text
-import com.varabyte.kobweb.compose.foundation.layout.Column
-import com.varabyte.kobweb.compose.ui.Alignment
-import com.varabyte.kobweb.compose.ui.Modifier
-import com.varabyte.kobweb.compose.ui.modifiers.borderRadius
-import com.varabyte.kobweb.compose.ui.modifiers.fillMaxWidth
-import com.varabyte.kobweb.compose.ui.modifiers.padding
-import com.varabyte.kobweb.compose.ui.modifiers.textAlign
-import com.varabyte.kobweb.compose.ui.modifiers.whiteSpace
-import com.varabyte.kobweb.core.Page
-import com.varabyte.kobweb.silk.components.icons.fa.FaMoon
-import com.varabyte.kobweb.silk.components.icons.fa.FaSun
-import com.varabyte.kobweb.silk.components.navigation.Link
-import com.varabyte.kobweb.silk.theme.colors.ColorMode
-import org.jetbrains.compose.web.css.percent
-import org.jetbrains.compose.web.css.px
-import org.jetbrains.compose.web.dom.Button
-import org.jetbrains.compose.web.dom.H1
-import org.jetbrains.compose.web.dom.Span
-
-
-@Page
-@Composable
-fun HomePage() {
-    Column(
-        Modifier
-            .fillMaxWidth().whiteSpace(WhiteSpace.PreWrap).textAlign(TextAlign.Center),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        var colorMode by ColorMode.currentState
-        Button(
-            onClick = { colorMode = colorMode.opposite },
-            Modifier.borderRadius(50.percent).padding(0.px).align(Alignment.End)
-        ) {
-            // Includes support for Font Awesome icons
-            if (colorMode.isLight) FaMoon() else FaSun()
-        }
-        H1 {
-            Text("Welcome to Kobweb!")
-        }
-        Span {
-            Text("Create rich, dynamic web apps with ease, leveraging ")
-            Link("https://kotlinlang.org/", "Kotlin")
-            Text(" and ")
-            Link(
-                "https://github.com/JetBrains/compose-multiplatform/#compose-html",
-                "Compose HTML"
-            )
-        }
-    }
-}*/
-
 
 
 import androidx.compose.runtime.*
-import com.varabyte.kobweb.compose.css.TextAlign
-import com.varabyte.kobweb.compose.css.WhiteSpace
+import com.varabyte.kobweb.core.Page
 import com.varabyte.kobweb.compose.foundation.layout.Column
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.*
-import com.varabyte.kobweb.core.Page
-import com.varabyte.kobweb.silk.components.icons.fa.FaMoon
-import com.varabyte.kobweb.silk.components.icons.fa.FaSun
-import com.varabyte.kobweb.silk.components.navigation.Link
-import com.varabyte.kobweb.silk.theme.colors.ColorMode
-import org.jetbrains.compose.web.css.percent
+import com.varabyte.kobweb.silk.components.forms.Input
+import org.jetbrains.compose.web.attributes.InputType
 import org.jetbrains.compose.web.css.px
+import org.jetbrains.compose.web.dom.Text
 import org.jetbrains.compose.web.dom.Button
 import org.jetbrains.compose.web.dom.H1
-import org.jetbrains.compose.web.dom.Span
-import org.jetbrains.compose.web.dom.Text   // ✅ yahi sahi import hai
 
 @Page
 @Composable
-fun HomePage() {
+fun AboutPage() {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
     Column(
-        Modifier
-            .fillMaxWidth()
-            .whiteSpace(WhiteSpace.PreWrap)
-            .textAlign(TextAlign.Center),
+        modifier = Modifier.fillMaxSize().padding(20.px),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        var colorMode by ColorMode.currentState
+        H1 { Text("About Page - Login Form") }
+
+        // Email Field
+        Input(
+            type = InputType.Email,
+            value = email,
+            onValueChange = { email = it },
+            modifier = Modifier.width(250.px).margin(bottom = 10.px),
+            placeholder = "Email address"
+        )
+
+        // Password Field
+        Input(
+            type = InputType.Password,
+            value = password,
+            onValueChange = { password = it },
+            modifier = Modifier.width(250.px).margin(bottom = 10.px),
+            placeholder = "Password"
+        )
+
+        // Login Button
         Button(
-            onClick = { colorMode = colorMode.opposite },
-            Modifier.borderRadius(50.percent).padding(0.px).align(Alignment.End)
+            attrs = {
+                onClick {
+                    println("Email: $email, Password: $password")
+                }
+            }
         ) {
-            if (colorMode.isLight) FaMoon() else FaSun()
-        }
-        H1 {
-            Text("Welcome to Kobweb!")
-        }
-        Span {
-            Text("Create rich, dynamic web apps with ease, leveraging ")
-            Link("https://kotlinlang.org/", "Kotlin")
-            Text(" and ")
-            Link("https://github.com/JetBrains/compose-multiplatform/#compose-html", "Compose HTML")
+            Text("Login")
         }
     }
 }
