7c420dffba3af3d2a71b37120207217d37df3f5b
vijaymaurya <vijaykushwahaji1415@gmail.com>
2025-08-22 18:35:55 +0530

    viewmodel

--- site/src/jsMain/kotlin/com/indusjs/statustracker/pages/ProfilePage.kt
+++ site/src/jsMain/kotlin/com/indusjs/statustracker/pages/ProfilePage.kt
@@ -1,8 +1,8 @@
 package com.indusjs.statustracker.pages
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
+import androidx.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
